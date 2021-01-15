a0e8dfae794c12a99c0f8babe700e968bfd0ef52
sptuan <sptuan@steinslab.io>
2021-01-15 18:42:06 +0800

    add basic model & REST router

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/sptuan/blog-service/internal/routers"
+	"net/http"
+	"time"
+)
 
-func main(){
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Pong!!!!!"})
-	})
+func main() {
+	router := routers.NewRouter()
 
-	r.Run()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
