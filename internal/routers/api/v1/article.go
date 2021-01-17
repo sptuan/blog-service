a241cf75beefb72ac4075066c19067c8a497ef26
sptuan <sptuan@steinslab.io>
2021-01-17 23:16:50 +0800

    add app basic response

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sptuan/blog-service/pkg/app"
+	"github.com/sptuan/blog-service/pkg/errcode"
+)
 
 type Article struct{}
 
@@ -8,7 +12,9 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context)    {}
+func (a Article) Get(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.NotFound)
+}
 func (a Article) List(c *gin.Context)   {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
